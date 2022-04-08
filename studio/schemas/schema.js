3c84cf237d4fbc192897e1f40b45ed504245b475
Anthony Ware <aware@buildmidwestern.com>
2022-04-08 14:32:57 -0500

    techHub-blog/Video 2 sanity schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,7 +3,14 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 /* eslint-enable */
 
+// import custom schemas
+import blog from './documents/blog';
+import author from './documents/author';
+
+// import custom objects
+import customImage from './objects/customImage';
+
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([]),
+  types: schemaTypes.concat([blog, author, customImage]),
 });
